aec63d683698d16a0cd9a271abc8554b50a8b069
lilianj <lilianj@kth.se>
2018-05-25 14:23:53 +0200

    kolla test.py i kommentarerna för vilka fel som finns atm

--- lab9/test.py
+++ lab9/test.py
@@ -19,7 +19,7 @@ class SyntaxTest(unittest.TestCase):
 
     # En grupp
     def testGroup(self):
-        self.assertEqual(kollaSyntax("Si(C3(COOH)2)4(H2O)7"), "Formeln är syntaktiskt korrekt") #funkar inte
+        self.assertEqual(kollaSyntax("Si(C3(COOH)2)4(H2O)7"), "Formeln är syntaktiskt korrekt") #"Maximum recursion depth exceeded"
 
     # En enkel atom
     def unknownAtom(self):
